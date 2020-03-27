01b3d5c89dbe6b150bef4124dd22532247d33287
chicori3 <40778768+chicori3@users.noreply.github.com>
2020-03-27 23:39:16 +0900

    JS works

--- drum_kit/drum.js
+++ drum_kit/drum.js
@@ -1,6 +1,18 @@
-window.addEventListener("keydown", function(a) {
-  const audio = this.document.querySelector(`audio[data-key="${a.keyCode}"]`);
-  if (!audio) return; // stop
+const keys = document.querySelectorAll(".key");
+
+function playSound(a) {
+  const audio = document.querySelector(`audio[data-key="${a.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${a.keyCode}"]`);
+  if (!audio) return;
   audio.currentTime = 0;
   audio.play();
-});
+  key.classList.toggle("playing");
+}
+
+function removeTransition(a) {
+  if (a.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
