abc7fb15d851fb6253dcc480567dd9e85ecf982e
Gitul <chicori3@naver.com>
2020-05-03 20:24:06 +0900

    Update index.js

--- side_in_on_scroll/index.js
+++ side_in_on_scroll/index.js
@@ -18,10 +18,8 @@ const sliderImages = document.querySelectorAll(".slide-in");
 
 function checkSlide() {
   sliderImages.forEach((sliderImage) => {
-    // half way through the image
     const slideInAt =
       window.scrollY + window.innerHeight - sliderImage.height / 2;
-    // bottom of the image
     const imageBottom = sliderImage.offsetTop + sliderImage.height;
     const isHalfShown = slideInAt > sliderImage.offsetTop;
     const isNotScrolledPast = window.scrollY < imageBottom;
